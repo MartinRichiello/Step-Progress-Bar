00b766dfb6ee67c11528183f95394a25aa6523dc
MartinRichiello <martinnicolasrichiello@gmail.com>
2025-03-25 12:45:26 +0100

    Javascript added

---
+++ public/js/script.js
@@ -0,0 +1,60 @@
+//Get the elements we need
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+
+//Variable to count active elements
+let currentActive = 1;
+
+//Check if we clikced button "Next"
+next.addEventListener("click", () => {
+
+    currentActive++
+
+    if(currentActive > circles.length){
+        currentActive = circles.length;
+    }
+    update()
+})
+
+//Check if we clikced button "Prev"
+prev.addEventListener("click", () =>{
+
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+//Checks the circles and which one of them should be updated
+function update(){
+
+    circles.forEach((circle, idx) => {
+
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        }else{
+            circle.classList.remove("active")
+        }
+    })
+
+    const actives = document.querySelectorAll(".active")
+
+    progress.style.width = (actives.length - 1) / (circles.length -1) * 100 + "%"
+
+
+    //Disable buttons in case pf beginning and end
+    if(currentActive === 1){
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    }else{
+        prev.disabled = false
+        next.disabled = false
+    }
+}
